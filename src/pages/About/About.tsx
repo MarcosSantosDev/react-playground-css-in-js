bc3a37f28a5105c9db5ff3cfaf1ed57e87cf486e
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2024-09-17 22:32:26 -0300

    fix: unused files

--- src/pages/About/About.tsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return <h2>Welcome to About!</h2>;
-}
-
-export default About;
--- src/pages/Home/Home.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h2>Welcome to home!</h2>;
-}
-
-export default Home;
